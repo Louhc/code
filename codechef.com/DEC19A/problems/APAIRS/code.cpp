7eb448664e811d4e54e4add79ca2d8893a37737b
Louhc <1098766736@qq.com>
2019-12-14 14:59:12 +0800

    louhc

---
+++ codechef.com/DEC19A/problems/APAIRS/code.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define i64 long long
+#define fp( i, b, e ) for ( int i(b), I(e); i <= e; ++i )
+#define fd( i, b, e ) for ( int i(b), I(e); i >= e; --i )
+
+int T;
+
+int main(){
+	cin >> T;
+	while( T-- ){
+	}
+	return 0;
+}
