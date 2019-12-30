b80121d4569752cdc6be6b335ffdef7bd16da856
Louhc <louhancheng@outlook.com>
2019-12-30 12:48:53 +0800

    louhc

--- a.cpp
+++ a.cpp
@@ -7,15 +7,8 @@ using namespace std;
 #define go( i, b ) for ( int i(b), v(to[i]); i; v = to[i = nxt[i]] )
 template<typename T> inline void cmax( T &x, T y ){ x < y ? x = y : x; }
 template<typename T> inline void cmin( T &x, T y ){ y < x ? x = y : x; }
-#define getchar() ( p1 == p2 && ( p1 = bf, p2 = bf + fread( bf, 1, 1 << 21, stdin ), p1 == p2 ) ? EOF : *p1++ )
-char bf[1 << 21], *p1(bf), *p2(bf);
-template<typename T>
-inline void read( T &x ){ char t(getchar()), flg(0); x = 0;
-        for ( ; !isdigit(t); t = getchar() ) flg = t == '-';
-        for ( ; isdigit(t); t = getchar() ) x = x * 10 + ( t & 15 );
-        flg ? x = -x : x;
-}
 
 int main(){
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 	return 0;
 }
